1ad957236d975a02d380df39cf67b13c19ec2eec
daslaller <bf2.dice@hotmail.com>
2025-09-05 13:17:27 +0200

    Add additional exports in synchronized_tracked_time_list.dart for Firestore and example files

--- lib/synchronized_tracked_time_list.dart
+++ lib/synchronized_tracked_time_list.dart
@@ -4,5 +4,8 @@
 library;
 
 export 'src/synchronized_tracked_set.dart';
+export 'src/firestore_service.dart';
+export 'src/realtime_example.dart';
+export 'src/trackedItemExample.dart';
 
 // TODO: Export any libraries intended for clients of this package.
